f18569707d54057a030574aa4dc87c64161d6bc6
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-02-27 02:46:01 +0200

    fix Dropdown bug

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -27,10 +27,10 @@ function Modal({ isOpen, closeModal, name, menuLinks }) {
         <>
             <Overlay isOpen={isOpen} onClose={closeModal}>
                 <nav
-                    className="bg-white xl-max:w-3/12 md-max:w-4/12 sm-max:w-[44%] sm-max:px-3 sm-max:text-base ml-auto flex h-full w-1/6 transform flex-col border-l-2 border-solid border-l-white/5 p-6 text-lg shadow-lg transition-transform duration-300 ease-linear "
+                    className="bg-white xl-max:w-3/12 md-max:w-4/12 sm-max:w-[44%] sm-max:px-3 sm-max:text-base ml-auto flex h-full w-1/6 transform flex-col p-6 text-lg shadow-lg transition-transform duration-300 ease-linear "
                     onClick={(e) => e.stopPropagation()}
                 >
-                    <ul className="border-b-2 border-solid ">
+                    <ul className="">
                         <li className="mb-4 w-full border-b border-solid border-[#494844] pb-5 font-medium text-white">
                             <div
                                 className="w-4/5 overflow-hidden overflow-ellipsis whitespace-nowrap text-primary"
@@ -65,30 +65,30 @@ function Modal({ isOpen, closeModal, name, menuLinks }) {
                             </li>
                         ))}
                     </ul>
-
-                    <div className="flex flex-col text-sm gap-y-1">
-                        <Button>Sign In</Button>
-                        <Button
-                            className={
-                                'bg-red-600 hover:bg-red-800'
-                            }
-                        >
-                            Or Log Out Function Implement
-                            Here
-                        </Button>
-                    </div>
-
                     {isSmallScreen ? (
-                        <div className="mt-2 shadow-xl flex pr-2">
+                        <div className="flex">
                             <DropdownInput
                                 className={
-                                    'right-4 mx-auto xs-max:w-[112px]'
+                                    'right-4 mx-auto xs-max:max-w-[100%]'
                                 }
                             />
                         </div>
                     ) : (
                         ''
                     )}
+                    <div className="flex flex-col text-sm gap-y-1 mt-2 font-normal">
+                        <Button className={'font-normal'}>
+                            Sign In
+                        </Button>
+                        <Button
+                            className={
+                                'bg-red-600 hover:bg-red-800 font-normal'
+                            }
+                        >
+                            Or Log Out Function Implement
+                            Here
+                        </Button>
+                    </div>
                 </nav>
             </Overlay>
         </>
